ae8f61590e634bacdea4c1844bd22c34d5133544
Divya R <kwrdivyaraj100@gmail.com>
2023-10-28 14:44:48 +0530

    Check for paper

--- app.js
+++ app.js
@@ -19,53 +19,68 @@ const game = () => {
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
         const hands = document.querySelectorAll(".hands img");
-    
+
         hands.forEach(hand => {
-          hand.addEventListener("animationend", function() {
-            this.style.animation = "";
-          });
+            hand.addEventListener("animationend", function () {
+                this.style.animation = "";
+            });
         });
-           
-    const computerOptions = ["rock", "paper", "scissors"];
 
-    options.forEach(option => {
-      option.addEventListener("click", function() {
-        
-        const computerNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[computerNumber];
+        const computerOptions = ["rock", "paper", "scissors"];
 
-      });
-    });
-};
+        options.forEach(option => {
+            option.addEventListener("click", function () {
 
-const compareHands = (playerChoice, computerChoice) => {
-    
-    const winner = document.querySelector(".winner");
-    
-    if (playerChoice === computerChoice) {
-      winner.textContent = "It is a tie";
-      return;
-    }
-    if (playerChoice === "rock") {
-        if (computerChoice === "scissors") {
-          winner.textContent = "Player Wins";
-         
-          return;
-        } else {
-          winner.textContent = "Computer Wins";
-         
-          return;
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+            });
+        });
+    };
+
+    const compareHands = (playerChoice, computerChoice) => {
+
+        const winner = document.querySelector(".winner");
+
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It is a tie";
+            return;
         }
-      }
+        if (playerChoice === "rock") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Player Wins";
+
+                return;
+            } else {
+                winner.textContent = "Computer Wins";
+
+                return;
+            }
+        }
+
+        if (playerChoice === "paper") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Computer Wins";
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins";
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
     }
 
-        
 
-    
-    
 
-        startGame();
-        playMatch();
+
+
+
+
+    startGame();
+    playMatch();
 };
 
 game();
